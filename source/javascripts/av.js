57b69d09d4893117168896f7c266537d8d1743dc
Gary A. Newsome <garyanewsome@gmail.com>
2017-06-26 13:23:28 -0400

    small changes remove unused Ubuntu font, resolve nonexistent conflict

--- source/javascripts/av.js
+++ source/javascripts/av.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('.motd').animatedHeadline({
     animationType: "rotate-1",
-    animationDelay: 2500,
+    animationDelay: 5500,
     barAnimationDelay: 3800,
     barWaiting: 800,
     lettersDelay: 50,
